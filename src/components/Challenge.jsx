03e4ba3b9976f9bddd52ad0fe6fcb0bfc7b4c9c8
John McCormick <johnmccormick91@gmail.com>
2024-03-08 17:08:25 +0000

    Removed formatted page number

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
 export default ({ challengeGroupIndex, challengeIndex, completed, pageNumber, name, points, updateChallengeList }) => {
-  const formattedPageNumber = pageNumber > 0 ? pageNumber : '';
   return (
     <div
       className='challenge'
       onClick={() => updateChallengeList(challengeGroupIndex, challengeIndex)}
     >
       <input className='checkbox' type='checkbox' checked={completed} onChange={() => {}}></input>
-      <span>{formattedPageNumber}</span>
+      <span>{pageNumber}</span>
       <span className={`challenge-name ${completed && 'strikethrough'}`}>{name}</span>
       <span>{points}</span>
     </div>
