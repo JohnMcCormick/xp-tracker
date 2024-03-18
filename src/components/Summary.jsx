d9af182d90c9031eeaf899e4f1d343ca6183e75d
John McCormick <johnmccormick91@gmail.com>
2024-03-18 12:50:17 +0000

    Filling level cells in sequence

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -13,12 +13,41 @@ const getCurrentLevel = (totalXP) => {
   return currentLevel;
 }
 
-const SummaryTable = () => {
-  return new Array(9).fill().map((_, i) => <SummaryLevel rows={9 - i} />)
+const SummaryTable = ({ totalXP }) => {
+  let remainingXP = totalXP;
+  return new Array(9).fill().map((_, levelIndex) => {
+    let level = levelIndex + 1;
+    let levelXP = level * 8 * 25;
+    let XPForLevel = remainingXP;
+    if (remainingXP >= levelXP) {
+      remainingXP -= levelXP;
+    } else {
+      remainingXP = 0;
+    }
+    return <div key={levelIndex} className='xp-table-level'><SummaryLevel remainingXP={XPForLevel} rows={level} /></div>
+  })
 }
 
-const SummaryLevel = ({ rows }) => {
-  return (<div className='xp-table-level'>{new Array(rows).fill().map(() => <div className='xp-table-row'>{(new Array(8)).fill().map(() => <div className='xp-table-cell'>25</div>)}</div>)}</div>)
+const SummaryLevel = ({ rows, remainingXP }) => {
+  return new Array(rows).fill().map((_, i) => {
+    let rowXP = 8 * 25;
+    let XPForLevel = remainingXP;
+    if (remainingXP >= rowXP) {
+      remainingXP -= rowXP
+    } else {
+      remainingXP = 0
+    }
+    return <div key={i} className='xp-table-row'><SummaryCells remainingXP={XPForLevel} /></div>
+  })
+}
+
+
+const SummaryCells = ({ remainingXP }) => {
+  return new Array(8).fill().map((_, i) => {
+    let filled = remainingXP >= 25;
+    if (filled) remainingXP -= 25;
+    return <div key={i} className={`xp-table-cell ${filled ? 'filled' : ''}`}>25</div>
+  })
 }
 
 export default ({ totalXP }) => {
@@ -31,7 +60,9 @@ export default ({ totalXP }) => {
       </div>
       <div className='xp-table'>
         <span>Additional XP: 0</span>
-        <SummaryTable />
+        <div className='xp-table-container'>
+          <SummaryTable totalXP={totalXP} />
+        </div>
       </div>
     </div>
   )
